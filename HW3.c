95ea572ecff54e7d5b6fa321fe10a3c332793bdb
kkwatinetz <142681281+kkwatinetz@users.noreply.github.com>
2024-09-11 14:44:58 -0400

    Update HW3.c

--- HW3.c
+++ HW3.c
@@ -3,5 +3,11 @@
 
 int main()
 {
+    int myArray[10] = { 1, 56 , 4 , 0 , -1 , 5 , 4 , 1 , 9 ,78};
     return 0;
 }
+/* Your code is supposed to print the following information:
+
+1)	Largest value
+2)	Average value as a float
+3)	Whether the array has duplicates or not */
